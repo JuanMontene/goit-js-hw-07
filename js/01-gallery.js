ce961665410115265787a89645723e219b0676d5
JuanMontene <juan.felipe.2001@hotmail.com>
2023-05-23 14:28:12 -0500

    Solución primer ejercicio tarea

---
+++ js/01-gallery.js
@@ -0,0 +1,36 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+const gallery = document.querySelector('.gallery'); // selecciona la clase gallery de HTML y lo asigna a la variable galleryRef
+
+const renderGallery = () => {
+  const galleryMarkup = galleryItems
+    .map(
+      ({ preview, original, description }) =>
+        `<li class="gallery__item">
+          <a class="gallery__link" href="${original}">
+            <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}">
+          </a>
+        </li>`,
+    )
+    .join('');
+
+  gallery.innerHTML = galleryMarkup;
+};
+
+const openModal = source => {
+  const instance = basicLightbox.create(`<img src="${source}">`);
+  instance.show();
+};
+
+gallery.addEventListener('click', event => {
+  event.preventDefault();
+  if (event.target.tagName === 'IMG') {
+    const source = event.target.getAttribute('data-source');
+    openModal(source);
+  }
+});
+
+renderGallery();
+
+console.log(galleryItems);

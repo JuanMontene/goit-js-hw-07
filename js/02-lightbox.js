ca235fe69e9491561e71d065b84222762e0ef50a
JuanMontene <juan.felipe.2001@hotmail.com>
2023-05-24 09:07:38 -0500

    finalizacion tarea 7

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,8 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+var lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 100,
+});
